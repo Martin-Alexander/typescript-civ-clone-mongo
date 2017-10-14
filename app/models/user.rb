20c565980659150eed469e137172cdc67b96eb45
Martin-Alexander <martingianna@gmail.com>
2017-10-14 18:56:17 -0400

    User#games method returns a criteria of games that the user is a player in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 class User
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :rememberable, authentication_keys: [:username]
 
-  ## Database authenticatable
   field :username,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
-
-  ## Rememberable
   field :remember_created_at, type: Time
-
-  has_many :players
 
   def email_required?; false; end
   def email_changed?; false; end
+
+  has_many :players
+
+  def games
+    Game.all.to_a.each_with_object(Mongoid::Criteria.new(Game)) do |game, criteria|
+      game.players.to_a.each { |player| criteria << game; break if player.user == self }
+    end
+  end
 end
