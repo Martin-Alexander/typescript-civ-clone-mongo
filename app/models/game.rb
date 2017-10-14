72bc9f73768b22284674108aa139d7176e5b7bfd
Martin-Alexander <martingianna@gmail.com>
2017-10-14 18:22:38 -0400

    Game and Player validataion

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,8 @@ class Game
   include Mongoid::Document
 
   has_many :players
+
+  field :state, type: String
+  
+  validates :state, inclusion: { in: ["lobby", "ongoing", "over", "paused"] }
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,10 @@ class Player
 
   belongs_to :user
   belongs_to :game
+
+  field :role, type: String, default: "player"
+  field :host, type: Boolean, default: false
+
+  validates :role, inclusion: { in: ["player", "dead_player", "observer"] }
+  validates :user, uniqueness: { scope: :game }
 end
