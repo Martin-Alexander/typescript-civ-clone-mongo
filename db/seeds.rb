4c36b4a65ede499331a80b98bfba20798cd41d9f
Martin-Alexander <martingianna@gmail.com>
2017-10-14 19:36:18 -0400

    seed now create a game and two players

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@ Game.destroy_all
 
 puts " - All users, players, and games destroyed"
 
-User.create! username: "martin", password: "123456"
-User.create! username: "sophie", password: "123456"
-User.create! username: "chloe", password: "123456"
+martin = User.create! username: "martin", password: "123456"
+sophie = User.create! username: "sophie", password: "123456"
+chloe = User.create! username: "chloe", password: "123456"
 
 puts " - Users created"
+
+new_game = Game.create! state: "lobby"
+
+Player.create! user: martin, game: new_game, host: true
+Player.create! user: sophie, game: new_game
+
+puts " - Game and players created"
 
 puts "Done"
