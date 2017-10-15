77d3b8a51e6950eab0d9c4b9d980a234de39e658
Martin-Alexander <martingianna@gmail.com>
2017-10-14 23:55:14 -0400

    Added method to counter number of players in a game User#number_of_player and a max player constant User::max_players

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,18 @@ class Game
   has_many :players
 
   field :state, type: String
-  
+
   validates :state, inclusion: { in: ["lobby", "ongoing", "over", "paused"] }
+
+  # Maximum number of players to a game
+  def self.max_players
+    6
+  end
+
+  # Returns the number of players (filter by role)
+  def number_of_players(filters = {})
+    counter = 0
+    players.each { |player| counter += 1 if filters[:role].nil? || filters[:role] == player.role }
+    counter
+  end
 end
