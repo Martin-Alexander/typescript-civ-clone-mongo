910e9cb765a93e33b7467ac17cdd51e515d32153
Martin-Alexander <martingianna@gmail.com>
2017-10-15 00:52:10 -0400

    PagesController#keep_user_in_lobby does exactly what it says

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
+  before_action :keep_user_in_lobby
+
   def home
     @games = Game.all
   end
 
   def lobby
+  end
+
+  private
+
+  def keep_user_in_lobby
+    if current_user.lobby && (params[:action] == "home" || params[:id] != current_user.lobby.id.to_s)
+      redirect_to lobby_path(current_user.lobby)
+    end
   end
 end
