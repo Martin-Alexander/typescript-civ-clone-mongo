b0ac164468a3085c00521ab64691f8054af0a0cd
Martin-Alexander <martingianna@gmail.com>
2017-10-15 01:00:56 -0400

    integrated join lobby with front end via LobbiesController#join

--- app/controllers/lobbies_controller.rb
+++ app/controllers/lobbies_controller.rb
@@ -3,4 +3,10 @@ class LobbiesController < ApplicationController
     current_user.leave_lobby
     redirect_to home_path
   end
+
+  def join
+    game = Game.find(params[:id])
+    current_user.join_game(game) if current_user.can_join_game?(game)
+    redirect_to lobby_path(game)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "pages#home", as: "home"
   get "/lobby/:id", to: "pages#lobby", as: "lobby"
   post "/lobby/leave", to: "lobbies#leave", as: "leave_lobby"
+  post "/lobby/:id/join", to: "lobbies#join", as: "join_lobby"
 end
