d980e0fff9c49db6ff31b4997bc42121a0321293
Martin-Alexander <martingianna@gmail.com>
2017-10-15 00:04:11 -0400

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,11 @@ chloe = User.create! username: "chloe", password: "123456"
 puts " - Users created"
 
 new_game = Game.create! state: "lobby"
+chloe_game = Game.create! state: "lobby"
 
 Player.create! user: martin, game: new_game, host: true
 Player.create! user: sophie, game: new_game
+Player.create! user: chloe, game: chloe_game, host: true
 
 puts " - Game and players created"
 
