9b60378cb120484ea8d824ae1b179e7b1712e26a
Martin Giannakopoulos <martingianna@gmail.com>
2017-10-16 15:51:03 -0400

    added route for LobbiesController#start and integrated with front end

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post "/lobby/leave", to: "lobbies#leave", as: "leave_lobby"
   post "/lobby/:id/join", to: "lobbies#join", as: "join_lobby"
   post "/player/:id/swap_role", to: "players#swap_role", as: "swap_role"
+  post "/lobby/:id/start", to: "lobbies#start", as: "start"
 end
