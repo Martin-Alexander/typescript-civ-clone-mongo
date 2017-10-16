cbc7454edc81c4d3a4f42529ac009a8fbea95c39
Martin Giannakopoulos <martingianna@gmail.com>
2017-10-16 16:40:20 -0400

    moved redirects to ApplicationController and began working on a redirect to keep users in their active game (similar to how user and always redirected back to whatever lobby they're in)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :keep_user_in_lobby
+  before_action :keep_user_in_game
+
+  def keep_user_in_lobby
+    if current_user && current_user.lobby && (params[:id] != current_user.lobby.id.to_s || params[:action] != "lobby")
+      redirect_to lobby_path(current_user.lobby)
+    end
+  end
+
+  def keep_user_in_game
+    if current_user && current_user.ongoing_game_playing_in (params[:id] != current_user.ongoing_game_playing_in.id.to_s || params[:action] != "show")
+      # TODO
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  before_action :keep_user_in_lobby
-
   def home
     @games = Game.all_lobbies
   end
@@ -9,13 +7,5 @@ class PagesController < ApplicationController
     @lobby = Game.find(params[:id])
     @players = @lobby.players.sort_by { |player| player.host ? 0 : 1 }
     @current_player = Player.where(game: @lobby, user: current_user).first
-  end
-
-  private
-
-  def keep_user_in_lobby
-    if current_user.lobby && (params[:action] == "home" || params[:id] != current_user.lobby.id.to_s)
-      redirect_to lobby_path(current_user.lobby)
-    end
   end
 end
