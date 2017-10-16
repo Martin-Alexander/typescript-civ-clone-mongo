6845f74e8319377e9a8c298e3e17fc824d6252cb
Martin Giannakopoulos <martingianna@gmail.com>
2017-10-16 15:44:29 -0400

    Game#ready_to_start? and Game#start to handle and validation game starting. Game::all_lobbies to query for all lobby games

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,6 +12,11 @@ class Game
     6
   end
 
+  # Returns all games that're in the 'lobby state'
+  def self.all_lobbies
+    Game.where(state: "lobby")
+  end
+
   # Returns the number of players (filter by role)
   def number_of_players(filters = {})
     counter = 0
@@ -22,5 +27,13 @@ class Game
   # Returns the host of the game as a user
   def host
     players.each { |player| return player.user if player.host }
+  end
+
+  def ready_to_start?
+    number_of_players(role: "player") > 1
+  end
+
+  def start
+    update!(state: "ongoing")
   end
 end
