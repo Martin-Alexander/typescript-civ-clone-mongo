b4828741a83a1046cb8f113dfe01f7f969d879fc
Martin Giannakopoulos <martingianna@gmail.com>
2017-10-17 14:01:08 -0400

    small bugfix in PagesController#game and added leave game button on game page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,6 @@ class PagesController < ApplicationController
   def game
     @game = Game.find(params[:id])
     @players = @game.players
-    @current_player = @players.find { |player| player.user = current_user }
+    @current_player = @players.select { |player| player.user = current_user }.first
   end
 end
