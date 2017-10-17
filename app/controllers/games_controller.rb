855cc29ca4df323ce5084153964a03966d38bde4
Martin Giannakopoulos <martingianna@gmail.com>
2017-10-17 15:56:24 -0400

    move start game logic from lobbies controller to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,10 @@
 class GamesController < ApplicationController
+  def start
+    game = Game.find(params[:id])
+    game.start if current_user && game.ready_to_start?
+    redirect_to game_path(game)
+  end
+    
   def leave
     game = Game.find(params[:id])
     game.player(current_user).role == "player" ? current_user.resign(game) : redirect_to(home_path)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post "/lobby/leave", to: "lobbies#leave", as: "leave_lobby"
   post "/lobby/:id/join", to: "lobbies#join", as: "join_lobby"
   post "/player/:id/swap_role", to: "players#swap_role", as: "swap_role"
-  post "/lobby/:id/start", to: "lobbies#start", as: "start"
+  post "/game/:id/start", to: "games#start", as: "start"
   get "/game/:id", to: "pages#game", as: "game"
   post "/game/:id/leave", to: "games#leave", as: "leave_game"
 end
