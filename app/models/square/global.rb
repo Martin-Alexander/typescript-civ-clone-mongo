26b13f8a5a78829e9af31cea9a0b37ae287edbf0
Martin-Alexander <martingianna@gmail.com>
2017-10-27 19:56:16 -0400

    added player feild to Square::Global for testing purposes

--- app/models/square/global.rb
+++ app/models/square/global.rb
@@ -3,5 +3,7 @@ module Square
     embedded_in :board, class_name: "Game"
     embeds_many :combat_units, class_name: "Unit::Combat"
     embeds_many :civilian_units, class_name: "Unit::Civilian"
+
+    field :player, type: Integer, default: 0
   end
 end
