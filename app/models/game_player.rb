e8a43774fdb524b5f5c7b7a43ea72097179d70d1
Martin-Alexander <martingianna@gmail.com>
2017-10-30 18:04:16 -0400

    Added number field to GamePlayer and player_number to Unit::Base

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -3,4 +3,6 @@ class GamePlayer
 
   embedded_in :game
   embeds_many :vision_squares, class_name: "Square::Vision"
+
+  field :number, type: Integer
 end
--- app/models/unit/base.rb
+++ app/models/unit/base.rb
@@ -1,5 +1,7 @@
 module Unit
   class Base
     include Mongoid::Document
+
+    field :player_number, default: 0
   end
 end
