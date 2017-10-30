bb7206c95f09f8e566b363096d1d1233782cef32
Martin-Alexander <martingianna@gmail.com>
2017-10-30 18:44:55 -0400

    add GamePlayer#units that returns all units belonging to the player and added Square::Global#units that returns all units the square

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -5,4 +5,14 @@ class GamePlayer
   embeds_many :vision_squares, class_name: "Square::Vision"
 
   field :number, type: Integer
+
+  # Searches through every square on the board and collects all units belonging
+  # to the player
+  def units
+    game.squares.each_with_object([]) do |square, collection|
+      square.units.each do |unit|
+        collection << unit if unit.player_number == number
+      end
+    end
+  end
 end
--- app/models/square/global.rb
+++ app/models/square/global.rb
@@ -3,7 +3,10 @@ module Square
     embedded_in :board, class_name: "Game"
     embeds_many :combat_units, class_name: "Unit::Combat"
     embeds_many :civilian_units, class_name: "Unit::Civilian"
-
-    field :player, type: Integer, default: 0
+    
+    # Returns a collection of all units in square
+    def units
+      combat_units + civilian_units
+    end
   end
 end
