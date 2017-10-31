4300050370877a09ff5c36ba2c9481065310df8c
Martin-Alexander <martingianna@gmail.com>
2017-10-30 22:04:03 -0400

    implemented zooming with mouse anchor

--- app/javascript/packs/event_handling.js
+++ app/javascript/packs/event_handling.js
@@ -5,7 +5,8 @@ const mouse = {
   down: false,
   positionOnLastDown: null,
   rawPosition: null,
-  rawIsoPosition: null
+  rawIsoPosition: null,
+  centerRelativePosition: null
 };
 
 window.addEventListener("mousedown", function() { 
@@ -31,20 +32,41 @@ window.addEventListener("mousemove", function(event) {
   setMousePosition(event);
 });
 
+window.addEventListener("wheel", function(event) {
+  const zoomSpeed = 1.05
+  if (event.deltaY < 0) {
+    UI.tileHeight *= zoomSpeed;
+    UI.tileWidth *= zoomSpeed;
+    UI.offset.x = (UI.offset.x * zoomSpeed) - (mouse.centerRelativePosition.x / 20);
+    UI.offset.y = (UI.offset.y * zoomSpeed) - (mouse.centerRelativePosition.y / 20);
+  } else {
+    UI.tileHeight /= zoomSpeed;
+    UI.tileWidth /= zoomSpeed;
+    UI.offset.x = (UI.offset.x / zoomSpeed) + (mouse.centerRelativePosition.x / 21);
+    UI.offset.y = (UI.offset.y / zoomSpeed) + (mouse.centerRelativePosition.y / 21);
+  }
+});
+
 function setMousePosition(event) {
   mouse.rawPosition = { 
     x: event.clientX, 
     y: event.clientY 
   };
 
+  mouse.centerRelativePosition = {
+    x: (mouse.rawPosition.x - (window.innerWidth / 2)),
+    y: (mouse.rawPosition.y - (window.innerHeight / 2))
+
+  }
+
   const offsetCoords = {
     x: mouse.rawPosition.x - UI.offset.x,
-    y: mouse.rawPosition.y - UI.offset.y
+    y: mouse.rawPosition.y - UI.offset.y - ((window.innerHeight - 15 * UI.tileHeight) / 2)
   };
 
   mouse.rawIsoPosition = { 
     x: ((offsetCoords.x - window.innerWidth / 2) + 2 * offsetCoords.y) / 2 ,
-    y: (2 * offsetCoords.y - (offsetCoords.x - window.innerWidth / 2)) / 2
+    y: (2 * offsetCoords.y - (offsetCoords.x - window.innerWidth / 2)) / 2 
   };
   
   UI.tileMousePosition = {
