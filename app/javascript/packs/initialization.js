a6b3b6006ebc7f7a5c185660ed003ec42db59dd1
Martin-Alexander <martingianna@gmail.com>
2017-11-01 18:38:35 -0400

    delete old javascript

--- app/javascript/packs/initialization.js
+++
@@ -1,5 +0,0 @@
-import "./game_data";
-import "./event_handling";
-import { renderBoard } from "./board_renderer";
-
-renderBoard(gameData, document.getElementById("canvas-container"));
--- app/javascript/packs/ui_state.js
+++
@@ -1,9 +0,0 @@
-const UI = {
-  tileHeight: 40,
-  tileWidth: 80,
-  offset: { x: 0, y: 0 },
-  tileMousePosition: {},
-  selectedTile: null
-};
-
-export { UI };
