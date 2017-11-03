199680d9c487822bf2deb36e6daaa7f76a937822
Martin-Alexander <martingianna@gmail.com>
2017-11-03 00:06:15 -0400

    updated embedded documents in Square::Global

--- app/models/square/global.rb
+++ app/models/square/global.rb
@@ -1,12 +1,14 @@
 module Square
   class Global < Base
     embedded_in :board, class_name: "Game"
-    embeds_many :combat_units, class_name: "Unit::Combat"
+    embeds_many :infantry, class_name: "Unit::Infantry"
+    embeds_many :tanks, class_name: "Unit::Tank"
+    embeds_many :ships, class_name: "Unit::Ship"
     embeds_many :workers, class_name: "Unit::Worker"
     
     # Returns a collection of all units in square
     def units
-      combat_units + workers
+      workers + infantry + tanks + ships
     end
   end
 end
