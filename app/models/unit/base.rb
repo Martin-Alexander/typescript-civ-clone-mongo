873e1bcb34e4795aa41389d54579ce702f56b3b5
Martin-Alexander <martingianna@gmail.com>
2017-11-05 03:24:34 -0500

    removed field 'units' from Square::Global means no more callback in units needed

--- app/models/unit/base.rb
+++ app/models/unit/base.rb
@@ -5,9 +5,7 @@ module Unit
     embedded_in :square, class_name: "Square::Global"
 
     field :player_number, default: 0
-
-    after_create :add_to_units
-
+    
     def move(square)
       unless square.id == _parent.id
         square.infantry << self.dup
@@ -18,11 +16,6 @@ module Unit
     def move_valid?(to_square)
       from_square = _parent
       true
-    end
-
-    def add_to_units
-      square.units << self
-      square.save
     end
   end
 end
