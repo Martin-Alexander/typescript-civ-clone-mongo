52e25d03edb09cb1f3d8480b0f7174008885d349
Martin-Alexander <martingianna@gmail.com>
2017-11-10 22:35:56 -0500

    mongoid module contains the logic nessecary to create a hash out of the game object (that is agnostic to it's stucture :D)

--- app/models/mongoid_model.rb
+++ app/models/mongoid_model.rb
@@ -1,5 +1,39 @@
 class MongoidModel
+	include Mongoid::Document
+
+	def self.direct_children(*args)
+		class_eval %Q(
+			def get_direct_children
+				#{args}
+			end			
+		)
+	end
+
+	def hashify_fields
+		output_hash = {}
+		fields.keys.each do |field_name| 
+			if field_name == "_id"
+				output_hash[:id] = id.to_s
+			elsif field_name == "_type"
+				output_hash[:type] = _type.split("::").last.downcase
+			else 
+				output_hash[field_name.to_sym] = send(field_name.to_sym)
+			end
+		end
+		output_hash
+	end
+
+	def children_types
+		_children.map { |em| em.class }.uniq
+	end
+
 	def to_hash
-		JSON.parse(to_json)
+		output = hashify_fields
+		if respond_to?(:get_direct_children)
+			get_direct_children.each do |child|
+				output[child] = send(child).map { |childs_child| childs_child.to_hash }
+			end
+		end
+		return output
 	end
 end
