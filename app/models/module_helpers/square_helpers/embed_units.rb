7fce398c30dd111946728035887b5a86c151db27
Martin-Alexander <martingianna@gmail.com>
2017-11-10 23:07:38 -0500

    added some documention to both model helper modules

--- app/models/module_helpers/square_helpers/embed_units.rb
+++ app/models/module_helpers/square_helpers/embed_units.rb
@@ -1,8 +1,12 @@
 module EmbedUnits
+  # Takes in a list of unit names as symbols
   def embed_units(*args)
     args.each do |unit|
+
+      # Embeds each unit (assuming a specific naming convention)
       embeds_many unit, class_name: "Unit::#{unit.to_s.singularize.capitalize}"
 
+      # Creates a 'create_#{unit_name}' method to create that unit
       Square::Global.class_eval %Q(
         def create_#{unit.to_s.singularize}(*args)
           #{unit}.create! args
@@ -10,6 +14,7 @@ module EmbedUnits
       )      
     end
 
+    # Returns as symbols all valid unit types
     Square::Global.class_eval %Q(
       def self.unit_types
         #{args.to_s}
--- app/models/module_helpers/square_helpers/units.rb
+++ app/models/module_helpers/square_helpers/units.rb
@@ -1,10 +1,12 @@
 module Units
+	# Returns an array of all units embedded in the square
 	def units
 		Square::Global.unit_types.each_with_object([]) do |type, array|
 			array << send(type)
 		end.flatten
 	end
 
+	# Given a unit type and list of properties, creates an embedded unit
 	def create_unit(unit, *args)
 		send(unit.to_s.pluralize.to_sym).create! args
 	end	
