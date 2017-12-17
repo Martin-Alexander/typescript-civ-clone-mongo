900aa6af719d3d7ba4c39103291205dc26a40dcb
Martin-Alexander <martingianna@gmail.com>
2017-12-17 02:32:40 -0500

    removed right-click menu and restricted mousedown and mouseup to left mouse button only

--- app/javascript/packs/app/event_router.js
+++ app/javascript/packs/app/event_router.js
@@ -19,20 +19,29 @@ function initializeEventListener(self) {
   const UI = self.UI;
   const inputController = self.inputController;
 
-  window.addEventListener("mousedown", function() { 
-    mouse.down = true; 
-    mouse.positionOnLastDown = {
-      x: mouse.rawPosition.x,
-      y: mouse.rawPosition.y
-    };
+  document.addEventListener("contextmenu", function(event) {
+    event.preventDefault(); 
+    return false; 
+  });
+
+  window.addEventListener("mousedown", function(event) {
+    if (event.button === 0) {
+      mouse.down = true;
+      mouse.positionOnLastDown = {
+        x: mouse.rawPosition.x,
+        y: mouse.rawPosition.y
+      };
+    } 
   });
 
   window.addEventListener("mouseup", function() { 
-    if (mouse.preDragDistance < 10) {
-      inputController.click();
+    if (event.button === 0) {
+      if (mouse.preDragDistance < 10) {
+        inputController.click();
+      }
+      mouse.preDragDistance = 0;
+      mouse.down = false; 
     }
-    mouse.preDragDistance = 0;
-    mouse.down = false; 
   });
 
   window.addEventListener("mousemove", function(event) {
