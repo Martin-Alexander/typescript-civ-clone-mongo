b4c96aa2554ce759cb03b8f2681dd6ace2ea8e9b
Martin-Alexander <martingianna@gmail.com>
2017-12-17 02:14:42 -0500

    implemented A*

--- lib/a_star/a_star.rb
+++ lib/a_star/a_star.rb
@@ -1,19 +1,66 @@
 class AStar
+  # Initialize A Star object and returns results of run
+  def self.run(game_object, square_objects)
+    AStar.new(game_object, square_objects).run
+  end
 
-	# Initialize A Star object and returns results of run
-	def self.run(game_object, square_objects)
-		AStar.new(game_object, square_objects).run
-	end
+  # Initialize an A Star object that is read to be run (shouldn't be called directly)
+  def initialize(game_object, square_objects)
+    @board = AbstractBoard.new(game_object)
+    @start = AbstractSquare.new(square_objects[:start])
+    @finish = AbstractSquare.new(square_objects[:finish])
+  end
 
-	# Initialize an A Star object that is read to be run (shouldn't be called directly)
-	def initialize(game_object, square_objects)
-		@board = AbstractBoard.new(game_object)
-		@start = AbstractSquare.new(square_objects[:start])
-		@finish = AbstractSquare.new(square_objects[:finish])
-	end
+  # Handle actual implementation of A Star
+  def run
+    closed_squares = []
+    opened_squares = [@start]
 
-	# Handle actual implementation of A Star
-	def run
-		puts "========== A STAR RUN ==================="
-	end
+    @start.current_path_cost = 0
+
+    while opened_squares.any?
+      current_square = opened_squares.sort do |a, b|
+        a.estimated_total_cost(@finish) <=> b.estimated_total_cost(@finish)
+      end.first
+
+      if current_square == @finish
+        return find_path(current_square)
+      end
+
+      closed_squares.push(current_square)
+      opened_squares.delete(current_square)
+
+      @board.neighbours(current_square).each do |neighbour|
+        if closed_squares.include?(neighbour)
+          next
+        end
+
+        if !opened_squares.include?(neighbour)
+          opened_squares.push(neighbour)
+        end
+
+        if current_square.current_path_cost + neighbour.move_cost >= neighbour.current_path_cost
+          next
+        end
+
+        neighbour.path_via = current_square
+        neighbour.current_path_cost = current_square.current_path_cost + neighbour.move_cost   
+      end
+    end
+  end
+
+  def find_path(square)
+    path = []
+    current_square = square
+
+    while !current_square.nil? 
+      path.prepend({
+        x: current_square.x,
+        y: current_square.y
+      })
+      current_square = current_square.path_via
+    end
+
+    return path
+  end
 end
--- lib/a_star/abstract_board.rb
+++ lib/a_star/abstract_board.rb
@@ -1,9 +1,38 @@
 class AbstractBoard
 	attr_reader :squares
-	def initialize(game_object)
-		@squares = []
-		game_object.squares.each do |square|
-			@squares << AbstractSquare.new(square)
-		end
-	end
+
+  def self.manhattan_distance(a, b)
+    (a.x - b.x).abs + (a.y - b.y).abs
+  end
+
+  def initialize(game_object)
+    @game = game_object
+    @squares = []
+    @game.squares.each do |square|
+      @squares << AbstractSquare.new(square)
+    end
+  end
+
+  def neighbours(square, radius = 1)
+    (-radius..radius).each_with_object([]) do |x_diff, array|
+      (-radius..radius).each do |y_diff|
+        unless (x_diff.zero? && y_diff.zero?) ||
+        x_diff + square.x < 0 || y_diff + square.y < 0 || 
+        x_diff + square.x > @game.board_size ||
+        y_diff + square.y > @game.board_size
+          array << find_square(x_diff + square.x, y_diff + square.y)
+        end
+      end
+    end    
+  end
+
+  private
+
+  def find_square(x, y)
+    if (x > @game.board_size || y > @game.board_size)
+      raise ArgumentError, "Invalid row #{y} or col #{x} for board size of #{@game.board_size}"
+    else
+      @squares[y * (@game.board_size + 1) + x]
+    end
+  end
 end
