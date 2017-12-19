bb22d4fe16d1ee23685925e9daf249dd3c13acb5
Martin-Alexander <martingianna@gmail.com>
2017-12-18 21:06:13 -0500

    eager loading

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,9 @@ module CivCloneMongo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths += %W(#{config.root}/app/models/module_helpers/game_helpers)
-    config.autoload_paths += %W(#{config.root}/app/models/module_helpers/square_helpers)
-    config.autoload_paths += %W(#{config.root}/lib/a_star)
+    config.eager_load_paths += %W(#{config.root}/app/models/module_helpers/game_helpers)
+    config.eager_load_paths += %W(#{config.root}/app/models/module_helpers/square_helpers)
+    config.eager_load_paths += %W(#{config.root}/lib/a_star)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
