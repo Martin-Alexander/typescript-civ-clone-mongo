401ff9cdba8e1aaacb5d5ef8e4db694fb7dda7cb
Martin-Alexander <martingianna@gmail.com>
2017-12-22 23:53:21 -0500

    Updated the backend move function to delegate down to unit type class, specifically for infantry

--- app/models/unit/base.rb
+++ app/models/unit/base.rb
@@ -4,20 +4,24 @@ module Unit
 
     field :player_number, default: 0
     
-    # Basic move implemenation
-    # TEST MODE
+    # Default move function
     def move(to_square)
       if move_valid?(to_square)
         to_square.infantry << self.dup
         self.delete
+        return true
+      else
+        return false
       end
     end
 
-    # Basic move validation
-    # TEST MODE
+    # Default move validation
     def move_valid?(to_square)
-      # square.neighbours.include?(to_square)
-      true
+      square.neighbours.include?(to_square)
+    end
+
+    def type
+      _type
     end
   end
 end
