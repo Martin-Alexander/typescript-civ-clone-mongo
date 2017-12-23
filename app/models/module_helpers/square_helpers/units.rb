e3b09909f66c98da6aa2fbeac3b6c0688582799c
Martin-Alexander <martingianna@gmail.com>
2017-12-22 23:19:52 -0500

    indentation with spaces

--- app/models/module_helpers/square_helpers/units.rb
+++ app/models/module_helpers/square_helpers/units.rb
@@ -1,13 +1,13 @@
 module Units
-	# Returns an array of all units embedded in the square
-	def units
-		Square::Global.unit_types.each_with_object([]) do |type, array|
-			array << send(type)
-		end.flatten
-	end
+  # Returns an array of all units embedded in the square
+  def units
+    Square::Global.unit_types.each_with_object([]) do |type, array|
+      array << send(type)
+    end.flatten
+  end
 
-	# Given a unit type and list of properties, creates an embedded unit
-	def create_unit(unit, *args)
-		send(unit.to_s.pluralize.to_sym).create! args
-	end	
+  # Given a unit type and list of properties, creates an embedded unit
+  def create_unit(unit, *args)
+    send(unit.to_s.pluralize.to_sym).create! args
+  end
 end
