a201ae2528836844d3135c5e0c45e5f0624bd7a6
Martin-Alexander <martingianna@gmail.com>
2018-01-08 02:36:23 -0500

    style tweak

--- app/javascript/packs/app/react_ui/components/TurnTimer.js
+++ app/javascript/packs/app/react_ui/components/TurnTimer.js
@@ -7,31 +7,29 @@ export default class TurnTimer extends React.Component {
       bottom: "50px",
       left: "calc(50% - 200px)",
       width: "400px",
-      height: "50px",
+      height: "100px",
       backgroundColor: "#e4e5d7"
     }
 
     const text = () => {
       if (this.props.ongoingTurnTransition) {
-        return "Next Turn. Please Wait..."
-      } else {
-        return "Ready"
+        return(<div>"Next Turn. Please Wait..."</div>)
       }
     }
     
     const playersReady = () => {
       return this.props.gamePlayers.map((gamePlayer) => {
         if (gamePlayer.turn_over) {
-          return(<span key={gamePlayer.number}>Player {gamePlayer.number} is <strong>ready</strong></span>)
+          return(<div key={gamePlayer.number}>Player {gamePlayer.number} is <strong>ready</strong> </div>)
         } else {
-          return(<span key={gamePlayer.number}>Player {gamePlayer.number} is <strong>not ready</strong></span>)
+          return(<div key={gamePlayer.number}>Player {gamePlayer.number} is <strong>not ready</strong> </div>)
         }
       });
     }
 
     return(
       <div style={turnTimerStyle}>
-        <div>{text()}</div>
+        {text()}
         <div>{playersReady()}</div>
       </div>
     );
