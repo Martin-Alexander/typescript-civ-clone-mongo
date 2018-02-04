f998d1a8c76b393e9e83b23e2d10c394bb563fc2
Martin-Alexander <martingianna@gmail.com>
2018-02-04 02:02:05 -0500

    fixed construction bug

--- app/models/unit/worker.rb
+++ app/models/unit/worker.rb
@@ -1,7 +1,7 @@
 module Unit
   class Worker < Base
     def create_structure(structure_name)
-      if Rules["structures"].keys.include?(structure_name)
+      unless Rules["structures"].keys.include?(structure_name)
         raise ArgumentError, "#{structure_name} is not a valid structure"
       end
       square.structures.create(type: structure_name, player_number: player_number)
