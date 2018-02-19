d32f35d8aaefc4d0de79e0ab677c547435b292af
Martin-Alexander <martingianna@gmail.com>
2018-02-19 17:16:57 -0500

    some rules refactoring

--- config/initializers/rules.rb
+++ config/initializers/rules.rb
@@ -1,31 +1,40 @@
-class Rules
-  def self.raw
-    YAML.load(File.open(File.join(Rails.root, "config/rules.yml")))
-  end
+class Rules 
+  module ClassMethods
+    def raw
+      YAML.load(File.open(File.join(Rails.root, "config/rules.yml")))
+    end
 
-  # Given a unit, the square it one, and the player who owns it: What are it's available orders
-  def self.orders_for_unit(unit)
-    case unit.type
-    when "worker"
-      orders_for_worker(unit)
-    else
-      raw["units"][unit.type]["allowed_orders"]
+    # Given a unit, the square it one, and the player who owns it: What are it's available orders
+    def orders_for_unit(unit)
+      case unit.type
+      when "worker"
+        orders_for_worker(unit)
+      else
+        raw["units"][unit.type]["allowed_orders"]
+      end
+    end
+
+    # Returns the type of a given order
+    def order_type(order)
+    end
+
+    # Returns the structure of a given construction order
+    def construction_order_structure(order)
+    end
+
+    private
+
+    def orders_for_worker(unit)
+      allowed_orders = []
+      raw["units"][unit.type]["allowed_orders"].each do |order|
+        if raw["orders"][order]["type"] == "construction" 
+        end
+      end
+    end
+
+    def validate_unit_square_and_player(unit)
     end
   end
 
-  # Returns the type of a given order
-  def self.order_type(order)
-  end
-
-  # Returns the structure of a given construction order
-  def self.construction_order_structure(order)
-  end
-
-  private
-
-  def self.orders_for_worker(unit)
-  end
-
-  def self.validate_unit_square_and_player(unit)
-  end
+  extend ClassMethods
 end
