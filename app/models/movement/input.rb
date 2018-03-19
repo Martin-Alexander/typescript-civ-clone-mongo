5d3e576170741909536efa2c96162444d2e1d224
Martin-Alexander <martingianna@gmail.com>
2018-03-19 01:56:04 -0400

    beginings of new movement system

---
+++ app/models/movement/input.rb
@@ -0,0 +1,20 @@
+module Movement
+  # Represents a users movement command
+  class Input
+    # Accepts the two pieces of information given by the user: the unit, and the path
+    def initialize(unit, path)
+    end
+
+    # Returns whether or not the immediate move can be executed
+    def valid?
+    end
+
+    # Updated database
+    def execute
+    end
+
+    # Returns information need by front end to animate move
+    def front_end_move_result
+    end
+  end
+end
